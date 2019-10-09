d3c7ed395a73e61fd568534d545d98310e8f7f9d
Hunter Raffety <hsraffety@gmail.com>
2019-10-09 15:52:43 -0700

    finished stock prices and started recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,28 @@
 import math
 
 def recipe_batches(recipe, ingredients):
+  # we need to compare the values of the matching keys
+  # in each dictionary that is coming into the function
+  # by looking through each corresponding pair of keys
+  # and evaluating the values to see if there are enough
+  # ingredients supplied as is required for the recipe.
+  # logic:
+  # if there are enough ingredients, how many batches
+  # can they make? Think about what if you have enough to
+  # make two batches worth of one ingredient but not of
+  # another.
+  recipe_requirements = set(recipe.values())
+  ingredients_requirements = set(ingredients.values())
+  test = set(recipe_requirements).intersection(set(ingredients_requirements))
+  for i in recipe_requirements:
+    print(i)
+  for j in ingredients_requirements:
+    print(j)
+  for k in test:
+    pass
+    # print(k)
   pass 
-
+  # these are not in the same order
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
